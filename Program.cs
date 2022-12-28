615a0da2a1d7d110985bd6c11f635fe24e2fbc2c
Marjiba Jamir <marjibajamir9@gmail.com>
2022-12-29 00:23:20 +0530

    Made new console

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace UserInputExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user for input
+            Console.Write("Enter your name: ");
+
+            // Read the user's input
+            string name = Console.ReadLine();
+
+            // Output the user's input
+            Console.WriteLine("Hello, " + name + "!");
+        }
+    }
+}
